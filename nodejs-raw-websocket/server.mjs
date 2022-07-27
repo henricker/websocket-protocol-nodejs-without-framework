ea14914f111c735917d2aa39e70347e39b5eaa42
henricker <henriquevieira@alu.ufc.br>
2022-07-27 20:50:58 -0300

    feat: receive messages until 125 bits length

--- nodejs-raw-websocket/server.mjs
+++ nodejs-raw-websocket/server.mjs
@@ -2,7 +2,17 @@ import { createServer } from 'http';
 import crypto from 'crypto';
 
 const PORT = 1337;
+const SEVEN_BITS_INTEGER_MARKER = 125;
+const SIXTEEN_BIT_INTEGER_MARKER = 126;
+const SIXTYFOUR_BIT_INTEGER_MARKER = 127;
+
+const MASK_KEY_BYTES_LENGTH = 4;
+
+// parseInt('10000000', 2)
+const FIRST_BIT = 128
+
 const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
 const server = createServer(
     (request, response) => {
         response.writeHead(200);
@@ -12,7 +22,7 @@ const server = createServer(
 .listen(PORT, () => console.log('Server listening to ', PORT));
 
 function onSocketUpgrade  (request, socket, head) {
-    const { 'sec-websocket-key': websocketClientKey } = request.headers;
+    const { 'sec-websocket-key': websocketClientKey } = request?.headers;
     const headers = prepareHandshake(websocketClientKey)
 
     /**
@@ -23,7 +33,8 @@ function onSocketUpgrade  (request, socket, head) {
         Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
         Sec-WebSocket-Version: 13
      */
-    socket.write(headers);
+    socket?.write(headers);
+    socket?.on('readable', () => onSocketReadable(socket));
 }
 
 function prepareHandshake(id) {
@@ -34,7 +45,7 @@ function prepareHandshake(id) {
         'Connection: Upgrade',
         `Sec-WebSocket-Accept: ${acceptKey}`,
         ''
-    ].map(line => line.concat('\r\n')).join('');
+    ]?.map(line => line.concat('\r\n')).join('');
 
     return headers
 }
@@ -43,6 +54,56 @@ function createSocketAccept(id) {
     const shaum = crypto.createHash('sha1');
     shaum.update(id + WEBSOCKET_MAGIC_STRING_KEY);
     return shaum.digest('base64');
+}
+
+function onSocketReadable(socket) {
+    if(!socket)
+        return
+    //consume optcode (first byte) => type of message (text, binary, ping, pong)
+    // 1 -> 1 byte = 8 bits
+    socket.read(1);
+
+    const [maskAndPayloadLength] = socket.read(1);
+    const payloadLength = maskAndPayloadLength - FIRST_BIT;
+
+    let messageLength = 0;
+
+    if(payloadLength <= SEVEN_BITS_INTEGER_MARKER) {
+        messageLength = payloadLength;
+    } else {
+        throw new Error('Payload length is too big');
+    }
+
+    //Get mask key (4 bytes) after payload length
+    const maskKey = socket.read(MASK_KEY_BYTES_LENGTH);
+
+    // The rest of the message (messageLength bytes) is a payload encoded
+    const encodedData = socket.read(messageLength);
+    const decodedData = umask(encodedData, maskKey).toString('utf8');
+    console.log(decodedData)
+}
+
+function umask(encodedData, maskKey) {
+
+    const finalBuffer = Buffer.from(encodedData);
+    // because the maskKey has only 4 bytes
+    // index % 4 === 0, 1, 2, 3 = index bits needed to decode the message
+
+    // XOR  ^
+    // returns 1 if both are different
+    // returns 0 if both are equal
+
+    // (71).toString(2).padStart(8, "0") = 0 1 0 0 0 1 1 1
+    // (53).toString(2).padStart(8, "0") = 0 0 1 1 0 1 0 1
+    //                                     0 1 1 1 0 0 1 0
+
+    // (71 ^ 53).toString(2).padStart(8, "0") = '01110010'
+    // String.fromCharCode(parseInt('01110010', 2))
+    for(let i = 0; i < finalBuffer.length; i++) {
+        finalBuffer[i] = encodedData[i] ^ maskKey[i % MASK_KEY_BYTES_LENGTH];
+    }
+
+    return finalBuffer;
 }
 
 /**
